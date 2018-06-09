5df6f12b59cdf5591fc60dcc0afd46070220901d
u71992mt <mariya.tsvarkaleva@student.manchester.ac.uk>
2018-06-09 13:00:10 -0700

    Initial commit

---
+++ my-app/src/Brands.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './index.css';
+
+class Brands extends Component {
+  render() {
+    return (
+    <div className="Brands">
+        <h2>Im a text, not a button :( </h2>
+
+          <div class="flex-grid">
+            <div class="col">Pic one</div>
+            <div class="col">Pic two</div>
+          /*  <div class="col"></div> */
+          </div>
+
+
+
+    </div>
+);
+  }
+}
+
+export default Brands;
