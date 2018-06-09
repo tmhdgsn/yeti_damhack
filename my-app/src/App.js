d1617e0b9710b4eb24077009c34717cecbf1ebfc
u71992mt <mariya.tsvarkaleva@student.manchester.ac.uk>
2018-06-09 16:48:56 -0700

    index page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Brands from './Brands';
+import Buttons from './Buttons';
+import 'typeface-roboto';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="flex-grid">
+          <div className="col">
+          <h2>Welcome to WhatsTheNameEven</h2>
+          </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <Brands />
+        <Buttons />
       </div>
     );
   }
---
+++ my-app/src/Buttons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    width: 300,
+    height: 80
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+function ContainedButtons(props) {
+  const { classes } = props;
+  return (
+    <div className='Buttons'>
+
+      <Button variant="contained" color="secondary" className={classes.button}
+      href="http://www.google.com" type="submit"
+      >
+        Next
+      </Button>
+
+    </div>
+  );
+}
+
+ContainedButtons.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ContainedButtons);
