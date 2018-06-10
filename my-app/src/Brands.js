764d2a285199d78381b6b94a10419fa83915f449
u71992mt <mariya.tsvarkaleva@student.manchester.ac.uk>
2018-06-09 17:12:22 -0700

    removed form

--- my-app/src/Brands.js
+++ my-app/src/Brands.js
@@ -21,65 +21,51 @@ class Brands extends Component {
 
   onClickAdidas(){
     this.setState({adidasSelected: !this.state.adidasSelected});
-    console.log(this.state.adidasSelected);
   }
 
   onClickHnM(){
     this.setState({hnmSelected: !this.state.hnmSelected});
-    console.log(this.state.hnmSelected);
+
   }
 
   onClickZara(){
     this.setState({zaraSelected: !this.state.zaraSelected});
-    console.log(this.state.zaraSelected);
   }
 
 
   render() {
     return (
       <div className="Brands">
-      <form className="registration-step-brands" method="POST" action="">
-      <input type="hidden" name="select" value=""/>
+
 
             <div className="flex-grid">
               <div className="col">
-                <ul className="brandsIconUl">
-                  <li>
+
                   <a onClick={this.onClickAdidas}>
                     <input type="hidden" name="brands" value="adidas" disabled=""/>
                     <img src={adidas} alt={"adidaslogo"} className={this.state.adidasSelected? 'img-border': ''}/>
                   </a>
-                  </li>
-                </ul>
+
               </div>
 
             <div className="col">
-              <ul className="brandsIconUl">
-                <li>
+
                 <a onClick={this.onClickHnM}>
                   <input type="hidden" name="brands" value="hnm" disabled=""/>
                   <img src={hnm} alt={"hnmlogo"} className={this.state.hnmSelected? 'img-border': ''}/>
                 </a>
-              </li>
-            </ul>
-          </div>
+
+            </div>
 
             <div className="col">
-              <ul className="brandsIconUl">
-                <li>
+
                 <a onClick={this.onClickZara}>
                   <input type="hidden" name="brands" value="hnm" disabled=""/>
                 <img src={zara} alt={"zaralogo"} className={this.state.zaraSelected? 'img-border': ''}/>
                 </a>
-              </li>
-            </ul>
           </div>
 
         </div>
-      </form>
-
-
-
       </div>
 
 
